d1ba48656f8be844ab0fee19ca527c0f93fde228
Aleksei Udovenko <aleksei@affine.group>
2024-06-30 12:36:31 +0200

    attempt to switch to pyomo (on present level learn 1.7s vs 70s)

--- optimodel/lp_oracle.py
+++ optimodel/lp_oracle.py
@@ -1,52 +1,62 @@
+import pyomo.environ as pyo
+from pyomo.opt import SolverFactory
+
 from monolearn import Oracle
 from monolearn.SparseSet import SparseSet
 
-from optisolveapi.milp import MILP
+# from optisolveapi.milp import MILP
 
 from optimodel.inequality import Inequality
+
+def pyo_dot(a, b):
+    assert len(a) == len(b)
+    lst = [a[i] * b[i] for i in range(len(b))]
+    return pyo.quicksum([a[i] * b[i] for i in range(len(b))])
 
 
 class LPbasedOracle(Oracle):
     def __init__(self, pool, solver=None):
         super().__init__()
 
-        self.solver = solver
-        self.n_calls = 0
-        self.milp = None
+        self.solver = solver or "glpk"
+        self.model = None
         self.pool = pool
 
     def _prepare_constraints(self):
-        self.milp = MILP.feasibility(solver=self.solver)
+        self.model = pyo.ConcreteModel()
+        self.model_solver = SolverFactory(self.solver)
+
+        # model.x = pyo.Var(range(V), domain=pyo.Binary)
+        # xs = list(model.x.values())
+
+        # model.OBJ = pyo.Objective(expr = pyo.summation(model.x))
+
+        # self.milp = MILP.feasibility(solver=self.solver)
+        LP = self.model = pyo.ConcreteModel()
 
         if self.pool.is_upper:
-            lb = 0  # monotone => nonnegative
+            # monotone => nonnegative
+            LP.xs = pyo.Var(range(self.pool.n), domain=pyo.NonNegativeReals)
+            LP.c = pyo.Var(domain=pyo.NonNegativeReals)
         else:
-            lb = None
+            LP.xs = pyo.Var(range(self.pool.n), domain=pyo.Reals)
+            LP.c = pyo.Var(domain=pyo.Reals)
 
-        # set ub = 1000+ ? ...
-        self.xs = []
-        for i in range(self.pool.n):
-            self.xs.append(self.milp.var_real("x%d" % i, lb=lb, ub=None))
-
-        self.c = self.milp.var_real("c", lb=lb, ub=None)
-        self.xsc = self.xs + [self.c]
-
+        LP.inc = pyo.ConstraintList()
         for p in self.pool.include:
             # ... >= c
             # ... -c >= 0
-            self.milp.add_constraint(
-                zip(self.xsc, p + (-1,)),
-                lb=0,
+            LP.inc.add(
+                pyo_dot(LP.xs, p) >= LP.c,
             )
 
         self.i2cs = []
         for q in self.pool.i2exc:
             # ... <= c - 1
             # ... -c <= -1
-            self.i2cs.append(dict(
-                coefs=tuple(zip(self.xsc, q + (-1,))),
-                ub=-1,
-            ))
+            self.i2cs.append(
+                pyo_dot(LP.xs, q) <= LP.c - 1,
+            )
 
     def _query(self, bads: SparseSet):
         assert isinstance(bads, SparseSet)
@@ -55,22 +65,22 @@ class LPbasedOracle(Oracle):
             ineq = Inequality((0,) * self.pool.n + (0,))
             return True, ineq
 
-        if self.milp is None:
+        if self.model is None:
             self._prepare_constraints()
 
-        self.n_calls += 1
-
-        LP = self.milp
-        cs = [LP.add_constraint(**self.i2cs[i]) for i in bads]
-        res = LP.optimize(log=0)
-        LP.remove_constraints(cs)
-
-        if res is False:
+        LP = self.model  # .clone()
+        LP.exc = pyo.ConstraintList()
+        for i in bads:
+            LP.exc.add(self.i2cs[i])
+        res = self.model_solver.solve(LP)
+        # del LP.exc
+        # print(res)
+        if res.solver.termination_condition != "optimal":
+            del LP.exc
             return False, None
 
-        sol = LP.solutions[0]
-        val_xs = tuple(sol[x] for x in self.xs)
-        val_c = sol[self.c]
+        val_xs = tuple(LP.xs[i].value for i in range(len(LP.xs)))
+        val_c = LP.c.value
 
         if not all(isinstance(v, int) for v in val_xs + (val_c,)):
             # if non-integral coefficients,
@@ -81,4 +91,5 @@ class LPbasedOracle(Oracle):
         ineq = Inequality(val_xs + (-val_c,))
         assert all(ineq.satisfy(p) for p in self.pool.include)
         assert all(not ineq.satisfy(self.pool.i2exc[i]) for i in bads)
+        del LP.exc
         return True, ineq
